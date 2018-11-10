baed0f7f5eba4f946b9e51282d3f031e1bfe931d
iameduard <iameduard@hotmail.com>
2018-11-10 12:46:33 -0400

    second commit

--- graficarFabrica.m
+++ graficarFabrica.m
@@ -1,28 +1,16 @@
-function [pa,pb]=graficarFabrica(num_iteraciones,num_graficas=1)
+function [pa,pb]=graficarFabrica(num_iteraciones)
   promedio=0;
-  p=simularFabricas(num_iteraciones);
-  Y=[p,0.025;1-p,1-0.025];
+  [p,error]=simularFabricas(num_iteraciones);
+  Y=[p,0.025];
   title('Ejercicio Fabricas');
   xlabel('Eventos');
   ylabel('Probabilidad');
   h = bar (Y);
-  set (h(1), "facecolor", "r")
-  set (h(2), "facecolor", "g")
-  %fig=figure();
-  %ax(1)=subplot(1,2,1);
-  %ax(2)=subplot(1,2,2); %segunda grafica..
-  %set(gca, 'xticklabelmode', 'manual');
-  set(gca, 'xticklabel',["P(Defecto)";"P(No Defecto)"]) ;
-  ylim([0 1]);
-  l = cell(1,2);
-  l{1}='Estimado'; l{2}='Real';    
-  legend(h,l);
+  grid on
+  set (h(1), "facecolor", "g")
+  title(strcat('Numero de Iteraciones:', num2str(num_iteraciones),' - Error: ',num2str(error)))
+  set(gca, 'xticklabel',["P(Defecto) Estimado ";"P(Defecto) Real"]) ;
+  ylim([0 .1]);
   pa=p;
   pb=1-p;
-  %text(1:length(Y),Y,num2str(Y'),'vert','bottom','horiz','center'); 
-  %box off
-  %set(gca,'xtick',xtick);
-  %xticklabel=["P(A|Dulce)";"P(B|Dulce)"];
-  %[~,o2]=plot(ax(2),ones(1,100)*0.025);
-  %plotyy([1 2],pa,1:100,pb);
 end
--- graficarFabricaSubPlot.m
+++ graficarFabricaSubPlot.m
@@ -1,36 +1,36 @@
 function [pa,pb]=graficarFabricaSubPlot(vector)
-  num_graficas=size(vector)(2)
-  disp(vector)
-  if num_graficas==1
-    num_iteraciones=vector
-    graficarFabrica(num_iteraciones)
-  else
-    %graficara solo 4 graficas
-    if num_graficas>4
-      num_graficas=4
-    endif
+    num_graficas=size(vector)(2)
+    if num_graficas==1
+      disp('Entro');
+      num_iteraciones=vector
+      graficarFabrica(num_iteraciones)
+    else
+      %Graficara solo 4 graficas..
+      if num_graficas>4
+        num_graficas=4
+      endif
     
-    for k=1:num_graficas
-      promedio=0;
-      p=simularFabricas(vector(k));
-      Y=[p,0.025;1-p,1-0.025];
-      xlabel('Eventos');
-      ylabel('Probabilidad');
-      subplot(2,2,k);
-      h = bar (Y);
-      set (h(1), "facecolor", "r")
-      set (h(2), "facecolor", "g")
-      %fig=figure();
-      title(strcat('Numero de Iteraciones:', num2str(vector(k))))
-      set(gca, 'xticklabel',["P(Defecto)";"P(No Defecto)"]) ;
-      ylim([0 1]);
-      %ax(2)=subplot(1,2,2); %segunda grafica..
-      %set(gca, 'xticklabelmode', 'manual');
-      %l = cell(1,2);
-      %l{1}='Estimado'; l{2}='Real';    
-      %legend(h,l);
-      pa=p;
-      pb=1-p;
-    endfor
-  endif
+      clf;
+      
+      for k=1:num_graficas
+        promedio=0;
+        [p,error]=simularFabricas(vector(k));
+        Y=[p,0.025];
+        xlabel('Eventos');
+        ylabel('Probabilidad');
+        subplot(2,2,k);
+        %.5 ancho de la barra
+        h = bar (Y,.5); 
+        grid on
+        colormap(summer(2));
+        size(h)
+        %set (h(1), "facecolor", "r")
+        set (h(1), "facecolor", "g")
+        title(strcat('Numero de Iteraciones:', num2str(vector(k)),' - Error: ',num2str(error)))
+        set(gca, 'xticklabel',["P(Defecto) Estimado";"P(Defecto) Real"]) ;
+        ylim([0 .1]);
+        pa=p;
+        pb=1-p;
+      endfor
+    endif
 end
