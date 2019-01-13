c8018929a91466b87775f02956882302e420501f
iameduard <iameduard@hotmail.com>
2019-01-13 10:58:38 -0400

    third commit

--- Tarea3_def.m
+++ Tarea3_def.m
@@ -18,22 +18,54 @@
  subplot(2,2,1);
  [V1,D1,Q1,Z1,Y1]=generar_normal_bivariable(S1,mu1,npuntos);
  scatter(Y1(1,:),Y1(2,:),'+');
+ title({"Fd Normal"});
  subplot(2,2,2);
  [V2,D2,Q2,Z2,Y2]=generar_normal_bivariable(S2,mu2,npuntos);
  scatter(Y2(1,:),Y2(2,:),'+');
+ title({"Fd Normal"});
  subplot(2,2,3);
  [V3,D3,Q3,Z3,Y3]=generar_normal_bivariable(S3,mu3,npuntos);
  scatter(Y3(1,:),Y3(2,:),'+');
+ title({"Fd Normal"});
  subplot(2,2,4);
  [V4,D4,Q4,Z4,Y4]=generar_normal_bivariable(S4,mu4,npuntos);
  scatter(Y4(1,:),Y4(2,:),'+');
+ title({"Fd Normal"});
   %hold off;
  %Ploteo de la distribucion teorica y la distribucion generada..
  range = linspace (-5, 5, 100);
  [X, Y] = meshgrid (range, range);
  subplot(1,2,1);
- Z = p_x_wi_multidimen2(X, Y,[1 0 ; 0 1],[0;0]);
- surf(X, Y, Z);
+ ZT1 = p_x_wi_multidimen2(X, Y,S1,mu1);
+ surf(X, Y, ZT1);
+ title({"SIGMA=[1 0;0 1] MU=[0;0]"});
  subplot(1,2,2);
  hist3(Z1');
+ title({"SIGMA=[1 0;0 1] MU=[0;0]"});
+ %
+ subplot(1,2,1);
+ ZT2 = p_x_wi_multidimen2(X, Y,S2,mu2);
+ surf(X, Y, ZT2);
+ title({"SIGMA=[1 0;0 1] MU=[1;1]"});
+ subplot(1,2,2);
+ hist3(Z2');
+ title({"SIGMA=[1 0;0 1] MU=[1;1]"});
+ %
+ subplot(1,2,1);
+ ZT3 = p_x_wi_multidimen2(X, Y,S3,mu3);
+ surf(X, Y, ZT3);
+ title({"SIGMA=[1 0.5 ; 0.5 1] MU=[0;0]"});
+ subplot(1,2,2);
+ hist3(Z3');
+ title({"SIGMA=[1 0.5 ; 0.5 1] MU=[0;0]"});
+ %
+ subplot(1,2,1);
+ ZT4 = p_x_wi_multidimen2(X, Y,S4,mu4);
+ surf(X, Y, ZT4);
+ title({"SIGMA=[1 0.5 ; 0.5 1] MU=[1;1]"});
+ subplot(1,2,2);
+ hist3(Z4');
+ title({"SIGMA=[1 0.5 ; 0.5 1] MU=[1;1]"});
+ %
+ %Clasificador Bayes.
  
