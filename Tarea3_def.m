61c33b3cf85f25dd36aa27e061a26f7424dc7d9e
iameduard <iameduard@hotmail.com>
2019-01-13 10:31:56 -0400

    second commit

--- Tarea3_def.m
+++ Tarea3_def.m
@@ -12,14 +12,28 @@
  mu2=[1,1];
  mu3=[0;0];
  mu4=[1,1];
+ npuntos=100;
  %Generar 100 muestras de cada clase...
- hold on;
- [V1,D1,Q1,Z1,Y1]=generar_normal_bivariable(S1,mu1,100);
+ %hold on;
+ subplot(2,2,1);
+ [V1,D1,Q1,Z1,Y1]=generar_normal_bivariable(S1,mu1,npuntos);
  scatter(Y1(1,:),Y1(2,:),'+');
- [V2,D2,Q2,Z2,Y2]=generar_normal_bivariable(S2,mu2,100);
- scatter(Y2(1,:),Y2(2,:),'o');
- [V3,D3,Q3,Z3,Y3]=generar_normal_bivariable(S3,mu3,100);
- scatter(Y3(1,:),Y3(2,:),'o');
- [V4,D4,Q4,Z4,Y4]=generar_normal_bivariable(S4,mu4,100);
- scatter(Y4(1,:),Y4(2,:),'o');
- hold off;
+ subplot(2,2,2);
+ [V2,D2,Q2,Z2,Y2]=generar_normal_bivariable(S2,mu2,npuntos);
+ scatter(Y2(1,:),Y2(2,:),'+');
+ subplot(2,2,3);
+ [V3,D3,Q3,Z3,Y3]=generar_normal_bivariable(S3,mu3,npuntos);
+ scatter(Y3(1,:),Y3(2,:),'+');
+ subplot(2,2,4);
+ [V4,D4,Q4,Z4,Y4]=generar_normal_bivariable(S4,mu4,npuntos);
+ scatter(Y4(1,:),Y4(2,:),'+');
+  %hold off;
+ %Ploteo de la distribucion teorica y la distribucion generada..
+ range = linspace (-5, 5, 100);
+ [X, Y] = meshgrid (range, range);
+ subplot(1,2,1);
+ Z = p_x_wi_multidimen2(X, Y,[1 0 ; 0 1],[0;0]);
+ surf(X, Y, Z);
+ subplot(1,2,2);
+ hist3(Z1');
+ 
--- p_x_wi_multidimen2.m
+++ p_x_wi_multidimen2.m
@@ -1,19 +1,17 @@
-function p=p_x_wi_multidimen2(x,y,a,c)
+function p=p_x_wi_multidimen2(x,y,sigma_i,mu_i)
   #p = (1/(sigma_i*sqrt(2*pi)))*exp(-0.5*(x-mu_i).^2/sigma_i.^2)
   %a=0;
   %b=1;
   %c=d=0;
   %p1=p2=0.5;
-  sigma_i=[1 c;c 1];
-  mu_i=[a;a];
   d= length(mu_i);
+  %m=10;
+  sigma_i_INV  = sigma_i**(-1);
+  determ_sigma = det(sigma_i);
+  m = 1 /sqrt((2*pi)**d * determ_sigma );
   for i=1:length(x)
     for j=1:length(y)
       xy=[x(i,j);y(i,j)];
-      m=10;
-      sigma_i_INV  = sigma_i**(-1);
-      determ_sigma = det(sigma_i);
-      #c = 1 /sqrt((2*pi)**d * determ_sigma )
       p(i,j) = m*exp(-0.5*(xy-mu_i)'*sigma_i_INV*(xy-mu_i));
     endfor
   endfor
